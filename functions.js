2ebd9ae92ce87555db7f15afc4cc006a08872445
Kyle Hardy <kyleahardy@gmail.com>
2024-02-23 17:05:30 -0500

    add game

---
+++ functions.js
@@ -0,0 +1,143 @@
+const numberOfSquares = 9;
+var gamesWonByO = 0;
+var gamesWonByX = 0;
+
+var oTurn = false;
+var xTurn = true;
+
+const xWinCondition = "XXX";
+const oWinCondition = "OOO";
+
+const empireImg = '<img class="board-image" src="img/empire.png" alt="empire"/>';
+const rebelsImg = '<img class="board-image" src="img/rebels.png" alt="rebels"/>';
+
+function clearGameBoard() {
+    for (let i = 1; i <= numberOfSquares; i++) {
+        let playerData = document.getElementById(`t${i}`);
+        playerData.innerHTML = "";
+        playerData.dataset.player = "";
+    }
+}
+
+function clickedSquare(elementId) {
+
+    console.log()
+    if (elementId === null) return;
+
+    const gameBoardValue = document.getElementById(elementId);
+
+    let newGameBoardValue = "";
+    let newGameBoardImg = "";
+
+    if (gameBoardValue.dataset.player.length > 0) {
+        return;
+    }
+
+    if (xTurn === true) {
+        gameBoardValue.dataset.player = "X"
+        newGameBoardImg = empireImg;
+        xTurn = false;
+        oTurn = true;
+    }
+    else {
+        gameBoardValue.dataset.player = "O";
+        newGameBoardImg = rebelsImg;
+        xTurn = true;
+        oTurn = false;
+    }
+
+    document.getElementById(elementId).innerHTML = newGameBoardImg;
+    checkForWinCondition();
+}
+
+function checkForWinCondition() {
+    // get game board state
+    const gameBoardValues = [];
+
+    for (let i = 1; i <= numberOfSquares; i++) {
+        let playerData = document.getElementById(`t${i}`);
+        let value = "";
+
+        if (playerData.dataset.player != null) {
+            value = playerData.dataset.player;
+        }
+        
+        gameBoardValues[i - 1] = value;
+    }
+
+    /* 
+    0 1 2
+    3 4 5
+    6 7 8
+    */
+
+    const winValues = [];
+    winValues[0] = `${gameBoardValues[0]}${gameBoardValues[1]}${gameBoardValues[2]}`;
+    winValues[1] = `${gameBoardValues[0]}${gameBoardValues[3]}${gameBoardValues[6]}`;
+    winValues[2] = `${gameBoardValues[0]}${gameBoardValues[4]}${gameBoardValues[8]}`;
+    winValues[3] = `${gameBoardValues[2]}${gameBoardValues[5]}${gameBoardValues[8]}`;
+    winValues[4] = `${gameBoardValues[2]}${gameBoardValues[4]}${gameBoardValues[6]}`;
+    winValues[5] = `${gameBoardValues[6]}${gameBoardValues[7]}${gameBoardValues[8]}`;
+    winValues[6] = `${gameBoardValues[3]}${gameBoardValues[4]}${gameBoardValues[5]}`;
+    winValues[7] = `${gameBoardValues[1]}${gameBoardValues[4]}${gameBoardValues[7]}`;
+
+    var wonMessage = "";
+    for (let i = 0; i < winValues.length; i++) {
+        if (winValues[i] === xWinCondition) {
+            gamesWonByX += 1;
+            wonMessage = "The Empire Wins!"
+            break;
+        }
+
+        if (winValues[i] === oWinCondition) {
+            gamesWonByO += 1;
+            wonMessage = "The Rebels Win!"
+            break;
+        }
+    }
+
+    if (wonMessage.length > 0) {
+        // allow screen to update before sending won message
+        setTimeout( function() {
+            displayModal(wonMessage);
+            clearGameBoard();
+        }, 300);
+
+        updatePlayerScores();
+    }
+}
+
+function updatePlayerScores() {
+    if (document.getElementById("score-x").innerText < gamesWonByX) {
+        document.getElementById("score-x").innerText = gamesWonByX;
+        addPointEffect("score-x");
+    }
+
+    if (document.getElementById("score-o").innerText < gamesWonByO) {
+        document.getElementById("score-o").innerText = gamesWonByO;   
+        addPointEffect("score-o");
+    }
+}
+
+function addPointEffect(playerId) {
+    document.getElementById(playerId).className = "animation flash";
+    setTimeout(function() {
+        document.getElementById(playerId).className = "";
+    }, 2000)
+}
+
+function displayModal(wonMessage) {
+    let winModal = document.getElementById("win-modal");
+    
+    winModal.removeChild(winModal.lastChild);    
+    winModal.className = "win-alert-modal display";
+
+    let winText = document.createElement("p");
+    
+    winText.innerText = wonMessage;
+    winModal.appendChild(winText);
+}
+
+function closeModal() {
+    document.getElementById("win-modal").className = "win-alert-modal hidden";
+}
---
+++ notes.md
@@ -0,0 +1,16 @@
+## features
+
+1. Avatars
+
+2. Multiplayer!
+    - Node.js
+    - Server
+
+3. Button Cosmetics
+    - add click effect
+    
+4. Score board cosmetics
+
+5. Player Win alert cosemtics
+
+6. Player win sound effects
